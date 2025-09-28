faea532fce24396bcf34cc325e23f243da23e2b7
Abrefa Joseph <josephabrefa@gmail.com>
2025-09-28 14:00:20 +0000

    Create index.js

---
+++ backend/client/src/index.js
@@ -0,0 +1,6 @@
+import App from './App.js';
+import { createElement } from 'react';
+import { createRoot } from 'react-dom/client';
+
+const root = createRoot(document.getElementById('root'));
+root.render(createElement(App));
